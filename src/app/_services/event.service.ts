7df375c5f99c2857471822277baa31499c341d3f
Oussama_Lachiheb <oussama.lachiheb@esprit.tn>
2023-04-24 00:39:49 +0100

    AddEvent

--- src/app/_services/event.service.ts
+++ src/app/_services/event.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Claims} from "../models/Claims";
-import {map} from "rxjs";
+import {map, Observable} from "rxjs";
 import {Shop} from "../models/shop";
 import {UserService} from "./user.service";
 import {User} from "../models/user";
+import {Event} from "../models/Event";
+import {Media} from "../models/media";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+  private apiUrl = 'http://localhost:8085/events'; // Replace with your API endpoint
+  event!:Event;
   token!:string;
   user!:User;
   constructor( private http : HttpClient,private userService : UserService) { }
@@ -27,5 +31,14 @@ export class EventService {
       .pipe(map((res:any)=>{
         return res;
       }))
+  }
+
+  addEvent(formData: FormData, mediaList: Media[]): Observable<Event> {
+    this.token = localStorage.getItem("currentUser")
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${this.token}`);
+
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.post<Event>(`${this.apiUrl}/addEvent`, formData, { headers });
   }
 }
